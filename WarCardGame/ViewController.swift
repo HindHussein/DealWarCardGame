788f4af4781b88a72bd81f76d27a4ddd13bfce8a
Hind Suleiman Hussein <hind.suleimanh@gmail.com>
2023-07-31 13:51:53 +0300

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Foothill on 31/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
