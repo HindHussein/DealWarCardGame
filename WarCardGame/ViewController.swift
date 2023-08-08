af11bb9ce1d47acbc7607d78513fc8ecff9bc366
Hind Suleiman Hussein <hind.suleimanh@gmail.com>
2023-08-08 10:01:48 +0300

    Try Commit

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if leftNumber > rightNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        
+        else if leftNumber < rightNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+        
+        else {
+            leftScoreLabel.text = String(leftScore)
+            rightScoreLabel.text = String(rightScore)
+            
+        }
+        
+    }
+    
 }
 
